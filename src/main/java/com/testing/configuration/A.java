dd40a6160fbc3e400de75e7b6c6188cda03daaf2
rahul <rahuljihelo@gmail.com>
2025-01-16 23:34:50 +0530

    Find Maxima and minima using stream api in java

--- src/main/java/com/testing/configuration/A.java
+++ src/main/java/com/testing/configuration/A.java
@@ -2,6 +2,7 @@ package com.testing.configuration;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class A {
 
@@ -14,8 +15,22 @@ public class A {
 
         /* Write a program to check if all
          elements in a list of strings are of the same length using Java Stream API.*/
-        List<String> name = Arrays.asList("Rahul", "Rohit", "Ranjt", "Visha");
+        List<String> name = Arrays.asList("Rahul", "Rohit", "Ranjit", "Vishal Hello");
         boolean b = name.stream().allMatch((e) -> e.length() ==name.get(1).length());
+        List<Integer> collect = name.stream().map(String::length).collect(Collectors.toList());
+        System.out.println(collect);
         System.out.println(b);
+        List<String> name2 = Arrays.asList("uuuu", "tttt");
+        boolean b1 = name2.stream().map(String::length).distinct().count() == 1;
+        System.out.println("This is "+b1);
+
+        /*Given a list of integers, write a program to find and print the
+        difference between the maximum and minimum numbers using Java Stream API.*/
+        List<Integer> no
+                = Arrays.asList(3, 5, 8, 2, 9, 4);
+        int max = no.stream().mapToInt(Integer::intValue).max().orElse(Integer.MIN_VALUE);
+        int min = no.stream().mapToInt(Integer::intValue).min().orElse(Integer.MAX_VALUE);
+        System.out.println("This is the difference between "+(max-min));
+
     }
 }
